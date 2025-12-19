c026d17e6435fb4416b783b87722b6f22a9ba444
MD Tushar Aman <138907416+TusharAmaan@users.noreply.github.com>
2025-12-19 18:05:57 +0000

    Delete conflicting app/page.tsx and update homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics} from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
